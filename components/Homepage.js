3f7ded09329dd163ab6e938bfb7a39c262c2c539
Vic3 <kkvic4@gmail.com>
2023-03-13 23:57:44 +0100

    first commit

---
+++ components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Web3Button } from '@web3modal/react'
+
+
+const Homepage = () => {
+  return (
+    <>
+    <Web3Button/>
+    
+    </>
+    )
+}
+
+export default Homepage
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+
+
+const chains = [arbitrum, mainnet, polygon]
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT
+const queryClient = new QueryClient()
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider
+})
+const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <WagmiConfig client={wagmiClient}>
+          <QueryClientProvider client={queryClient}>
+    
+     <Component {...pageProps} />
+     </QueryClientProvider>
+     </WagmiConfig>
+    </>
+ 
+  )
 }
